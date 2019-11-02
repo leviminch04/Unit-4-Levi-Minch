456830fb35c93f1180abf7828b69189a8af52318
Levi Minch <levi@Levis-MacBook-Pro.local>
2019-11-02 01:06:13 -0700

    Create Child.java

---
+++ Child.java
@@ -0,0 +1,14 @@
+public class Child{
+  int age;
+  public int Child(int declaredAge){
+    age = declaredAge;
+    return age;
+  }
+  public int setAge(int newAge){
+    age = newAge;
+    return age;
+  }
+  public int getAge(){
+    return age;
+  }
+}
